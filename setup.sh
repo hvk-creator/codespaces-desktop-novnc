025f5b039c881fc8764bfd1b4e00dd7abb9a93e0
hvk-creator <hvk261208@proton.me>
2025-02-09 20:42:04 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
-
+sudo apt update
+sudo apt install tigervnc-standalone-server -y
+sudo apt install xfce4 xfce4-goodies -y
+git clone https://github.com/novnc/noVNC
+cd noVNC
+sudo vncserver -SecurityType none -xstartup "xfce4-session" -r
+./utils/novnc_proxy --vnc 127.0.0.1:5901 --listen localhost:6080
