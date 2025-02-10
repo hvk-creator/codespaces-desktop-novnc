fa294bf97801a708f063f83f72c82a65a88e9406
hvk-creator <hvk261208@proton.me>
2025-02-10 18:05:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-cd ~/noVNC
+cd /workspace/codespaces-desktop-novnc/noVNC
 sudo vncserver -SecurityType none -xstartup "xfce4-session" -r #If you are running this in Gitpod, remove `-SecurityType none` and `-r`.
 ./utils/novnc_proxy --vnc 127.0.0.1:5901 --listen localhost:6080
