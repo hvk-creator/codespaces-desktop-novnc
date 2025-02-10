8b7f836b1ccc2cc12d6ccc69fa320e1cee067055
hvk-creator <hvk261208@proton.me>
2025-02-10 18:04:46 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ sudo apt update
 sudo apt install tigervnc-standalone-server -y
 sudo apt install xfce4 xfce4-goodies -y
 git clone https://github.com/novnc/noVNC
-cd ~/noVNC
+cd /workspace/codespaces-desktop-novnc/noVNC
 sudo vncserver -SecurityType none -xstartup "xfce4-session" -r #If you are running this in Gitpod, remove `-SecurityType none` and `-r`. Type the password if prompted (1st time installation only).
 ./utils/novnc_proxy --vnc 127.0.0.1:5901 --listen localhost:6080
